6ff8139d586b9d2cf361d3345a234d5106b6e0c8
marioaje <marioaje@gmail.com>
2026-01-06 14:54:22 -0600

    Update funciones.py

--- Semana 1/funciones.py
+++ Semana 1/funciones.py
@@ -13,3 +13,10 @@ unavariable = funcionRetornando(12,9)
 print( unavariable )
 
 print (funcionRetornando(12,9))
+
+
+def funcionConVariosArgumentos(nombre, edad):
+    return f" Soy {nombre} y mi edad es: {edad} "
+
+
+print( funcionConVariosArgumentos("Mario", "12") )
